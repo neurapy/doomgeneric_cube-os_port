9d572a011de0fcc3f0a745c9fb97a2c01136ac1e
Gerrit Dolderer <gerrit.dolderer@hey.com>
2026-03-12 22:19:41 +0100

    doom ctrl

--- doomgeneric/doomgeneric.h
+++ doomgeneric/doomgeneric.h
@@ -38,6 +38,7 @@ void	 DG_SleepMs(uint32_t ms);
 uint32_t DG_GetTicksMs();
 int	 DG_GetKey(int *pressed, unsigned char *key);
 void	 DG_SetWindowTitle(const char *title);
+int	 DG_GetNativePixelFormat(uint32_t *depth, uint32_t *pixel_order);
 
 #ifdef __cplusplus
 }
